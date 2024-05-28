158ae05a5613449157f4fcc2daef1522608ea4ae
Stephan Griesel <sgriesel@gmail.com>
2024-05-28 06:37:37 +0200

    nav route set up

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,11 @@
   import "../app.css";
 </script>
 
+<div class="bg-base-100 justify-between fixed top-0 left-0 right-0">
+  <div class="navbar max-w-3xl mx-auto justify-between">
+    <div><a href="/" class="btn btn-ghost text-xl">Linkje</a></div>
+    <div><a href="/user" class="btn btn-ghost">My Page</a></div>
+  </div>
+</div>
+
 <slot />
