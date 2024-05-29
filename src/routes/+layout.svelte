403f21b191f7d21e453b30a52658bb7c7d8a5469
Stephan Griesel <sgriesel@gmail.com>
2024-05-29 07:29:04 +0200

    layout update & click event

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,17 @@
 <script>
   import "../app.css";
+  import { goto } from "$app/navigation";
 </script>
 
 <div class="bg-base-100 justify-between fixed top-0 left-0 right-0">
   <div class="navbar max-w-3xl mx-auto justify-between">
-    <div><a href="/" class="btn btn-ghost text-xl">Linkje</a></div>
-    <div><a href="/user" class="btn btn-ghost">My Page</a></div>
+    <div>
+      <a href="/" class="btn btn-ghost text-xl">Linkje</a>
+      <a href="/user" class="btn btn-ghost">My Page</a>
+    </div>
+    <div>
+      <button on:click={() => goto("/login")}>Login</button>
+    </div>
   </div>
 </div>
 
