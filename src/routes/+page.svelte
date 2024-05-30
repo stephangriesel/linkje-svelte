ab6a3fb6f86ab8b762e4361ab2f502a0bc0bb00a
Stephan Griesel <sgriesel@gmail.com>
2024-05-30 06:23:58 +0200

    login page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<div class="hero bg-base-300">
+  <div class="hero-content">
+    <div class="max-w-md text-center">
+      <h1>Linkje</h1>
+      <p>Create account and personalise</p>
+      <a href="/login" class="btn btn-primary">Create account</a>
+    </div>
+  </div>
+</div>
