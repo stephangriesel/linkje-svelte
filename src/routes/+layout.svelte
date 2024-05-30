95f4b0d27959b93e530cce7305aadae00cbe9b73
Stephan Griesel <sgriesel@gmail.com>
2024-05-30 06:23:30 +0200

    layout & path

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,14 +3,16 @@
   import { goto } from "$app/navigation";
 </script>
 
-<div class="bg-base-100 justify-between fixed top-0 left-0 right-0">
-  <div class="navbar max-w-3xl mx-auto justify-between">
+<div class="bg-base-100 fixed top-0 left-0 right-0">
+  <div class="navbar flex justify-between max-w-3xl mx-auto">
     <div>
       <a href="/" class="btn btn-ghost text-xl">Linkje</a>
       <a href="/user" class="btn btn-ghost">My Page</a>
     </div>
     <div>
       <button on:click={() => goto("/login")}>Login</button>
+      <span>user@domain.com</span>
+      <button on:click={() => console.log("logging out")}>Log out</button>
     </div>
   </div>
 </div>
